ce31e2ee7382f5f7f812c291a69b3fecfce774cd
leyla_qiao <775177772@qq.com>
2022-06-25 00:07:38 +0800

    UPG: update then

--- promise/1.basic.js
+++ promise/1.basic.js
@@ -28,7 +28,7 @@ promise.then((value) => {
 
 // 针对then的return值做不同处理
 // 1. onFulFilled/onRejected 返回的不是promise，return的是普通值，那么这个值会被传递到下一个then的成功回调（onFulFilled）中
-// 2. onFulFilled/onRejected 返回的是promise，则根据返回的promise 执行的resolve/reject来决定走成功还是失败
+// 2. onFulFilled/onRejected 返回的是promise，则根据（onFulFilled或onRejected）返回的promise 执行的resolve/reject来决定走成功还是失败，执行resolve则走下一个then的成功，执行reject则走下一个then的失败，无论执行的是本次then的onFulFilled还是onRejected
 // 3. 若想要走到下一个then的onRejected中，可以在本次then的成功回调（onFulFilled）或失败回调（onRejected）中抛出异常
 // 总结：要么抛错，要么返回一个失败的promise，其他都走成功
 // ps: 如果返回的是一个pending的promise，则会中断promise链
